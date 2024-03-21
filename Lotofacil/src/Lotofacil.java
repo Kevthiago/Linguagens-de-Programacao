feacd3c34128f81e46303836b7809dbe3b793f4a
Kevthiago <145407238+Kevthiago@users.noreply.github.com>
2024-03-21 12:38:18 -0300

    melhorias

--- Lotofacil/src/Lotofacil.java
+++ Lotofacil/src/Lotofacil.java
@@ -1,74 +1,90 @@
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.InputMismatchException;
+
 public class Lotofacil {
     public static void main(String[] args) {
+
         Scanner scn = new Scanner(System.in);
         Random rdn = new Random();
 
         int opcao = 0;
 
-        try {
-            do {
-                //Criação do Menu Lotofacil
-                System.out.println("**********************************");
-                System.out.println("MENU LOTOFÁCIL");
-                System.out.println("1) Apostar de 0 a 100: ");
-                System.out.println("2) Apostar de A a Z: ");
-                System.out.println("3) Apostar em par ou ímpar: ");
-                System.out.println("0) Sair ");
-                System.out.println("**********************************");
-                System.out.println("Escolha uma opção: ");
-                opcao = scn.nextInt();
+        //verificar entrada
+        boolean entradaValida = false;
 
-                switch (opcao) {
-                    case 1:
-                        int numLido = 0, numRandom = 0;
-                        numRandom = rdn.nextInt(101); //gera um número aleatório entre 0 e 100
-                        System.out.println("Escolha um número entre 0 e 100: ");
-                        numLido = scn.nextInt();
+        while (!entradaValida ) {
+            try {
+                do {
+                    //Criação do Menu Lotofacil
+                    System.out.println("**********************************");
+                    System.out.println("MENU LOTOFÁCIL");
+                    System.out.println("1) Apostar de 0 a 100: ");
+                    System.out.println("2) Apostar de A a Z: ");
+                    System.out.println("3) Apostar em par ou ímpar: ");
+                    System.out.println("0) Sair ");
+                    System.out.println("**********************************");
+                    System.out.println("Escolha uma opção: ");
+                    opcao = scn.nextInt();
 
-                        if (numLido >= 0 && numLido <= 100) {
-                            if (numLido == numRandom) {
-                                System.out.println("Parabéns, você ganhou R$1000,00!");
+                    switch (opcao) {
+                        case 1:
+                            int numLido = 0, numRandom = 0;
+                            numRandom = rdn.nextInt(101); //gera um número aleatório entre 0 e 100
+                            System.out.println("Escolha um número entre 0 e 100: ");
+                            numLido = scn.nextInt();
+
+                            if (numLido >= 0 && numLido <= 100) {
+                                if (numLido == numRandom) {
+                                    System.out.println("Parabéns, você ganhou R$1000,00!");
+                                } else {
+                                    System.out.println("Que pena. O número sorteado foi: " + numRandom);
+                                }
+                            } else {
+                                System.out.println("Aposta inválida! Deve ser um número entre 0 e 100.");
                             }
-                            else {
-                                System.out.println("Que pena. O número sorteado foi: " + numRandom);
+                            break;
+                        case 2:
+                            char letraPremiada = 'K'; // Letra premiada
+                            char letra;
+                            System.out.println("Escolha uma letra de A à Z");
+                            letra = scn.next().toUpperCase().charAt(0); // Lê a entrada do usuário, converte para maiúsculas e pega o primeiro caractere
+                            if (Character.isLetter(letra)) {
+                                if (letra == letraPremiada) {
+                                    System.out.println("Parabéns! Você ganhou R$500,00.");
+                                } else {
+                                    System.out.println("Que pena! A letra sorteada foi: " + letraPremiada);
+                                }
+                            } else {
+                                System.out.println("Aposta inválida! Deve ser uma letra entre A e Z.");
                             }
-                        } else {
-                            System.out.println("Aposta inválida! Deve ser um número entre 0 e 100.");
-                        }
-                        break;
-                    case 2:
-                        char letraPremiada = 'K'; //Letra premiada
-                        char letra;
-                        System.out.println("Escolha uma letra de A à Z");
-                        letra = (char) Character.toUpperCase(System.in.read());
-                        if (Character.isLetter((char) letra)){
-                            if (letra == letraPremiada){
-                                System.out.println("Parabéns! Você ganhou R$500,00.");
-                            }else{
-                                System.out.println("Que pena! A letra sorteada foi: " + letraPremiada);
+                            break;
+                        case 3:
+                            int num = 0;
+                            System.out.println("Escolha um número inteiro aleatório: ");
+                            num = scn.nextInt();
+                            if (num % 2 == 0) {
+                                System.out.println("Parabéns! Você ganhou R$100,00.");
+                            } else {
+                                System.out.println("Que pena! O número digitado é ímpar e a premiação foi para números pares.");
                             }
-                        } else {
-                            System.out.println("Aposta inválida! Deve ser uma letra entre A e Z.");
-                        }
-                        break;
-                    case 3:
-                        int num = 0;
-                        System.out.println("Escolha um número inteiro aleatório: ");
-                        num = scn.nextInt();
-                        if (num % 2 == 0){
-                            System.out.println("Parabéns! Você ganhou R$100,00.");
-                        } else {
-                            System.out.println("Que pena! O número digitado é ímpar e a premiação foi para números pares.");
-                        }
-                        break;
-                    case 0:
-                        break;
-                    default:
-                        System.out.println("Comando inválido!");
-                }
-            } while (opcao != 0);
-        } catch (Exception e){}
+                            break;
+                        case 0:
+                            System.out.println("Encerrando programa...");
+                            break;
+                        default:
+                            System.out.println("Comando inválido!");
+                    }
+                } while (opcao != 0);
+
+                entradaValida = true; //BOOLEAN COMO TRUE PARA SAIR DO LOOP DE VERIFICAÇÃO
+
+            } catch (InputMismatchException e) {
+                System.err.println("Entrada inválida." + e.getMessage()); //MENSAGEM DE ERRO
+                //scn.nextLine(); //LIMPA O BUFFER DA ENTRADA
+                break;
+            }
+        }
     }
 }
